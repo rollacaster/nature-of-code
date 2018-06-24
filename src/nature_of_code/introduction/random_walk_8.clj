baedb69eb4063560b97d1a13389745a1c6913bd6
Thomas Sojka <thomas.sojka@comsysto.com>
2018-06-24 10:57:56 +0200

    Add random-walk (8 directons)

---
+++ src/nature_of_code/introduction/random_walk_8.clj
@@ -0,0 +1,21 @@
+(ns nature-of-code.introduction.random-walk-8
+  (:require [quil.core :as q]))
+
+(defn setup []
+  (def walker (atom (hash-map :x (/ (q/width) 2) :y (/ (q/height) 2))))
+  (q/background 255))
+
+(defn draw []
+  (q/stroke 0)
+  (let [stepx (- (rand-int 3) 1)
+        stepy (- (rand-int 3) 1)]
+    (swap! walker (fn [{:keys [x y]}] {:x (+ x stepx) :y (+ y stepy)})))
+  (let [{:keys [x y]} @walker]
+    (q/point x y)))
+
+(q/defsketch random-walk-8
+  :title "Random Walk 8 Directions"
+  :settings #(q/smooth 2)
+  :setup setup
+  :draw draw
+  :size [500 500])
