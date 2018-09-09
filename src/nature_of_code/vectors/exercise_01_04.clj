fa05b3543b79ede8b66f00c747580ee4100aa5c7
Thomas Sojka <thomas.sojka@comsysto.com>
2018-09-09 10:15:40 +0200

    Add exercises 1.4-1.8

---
+++ src/nature_of_code/vectors/exercise_01_04.clj
@@ -0,0 +1,12 @@
+(ns nature-of-code.vectors.exercise-01-04)
+
+(defn normalize [v]
+  (let [m (mag v)]
+    (when (not (= m 0)) (div v m))))
+
+(defn mag [[x y]] (Math/sqrt (+ (* x x) (* y y))))
+
+(defn limit [[x y] top]
+  (if (> (mag [x y]) top)
+    (mult (normalize [x y]) top)
+    [x y]))
