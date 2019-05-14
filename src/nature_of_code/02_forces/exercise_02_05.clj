49376cdaa7b1b8761f6f50b0c3d9413048022619
Thomas Sojka <thomas.sojka@comsysto.com>
2019-05-14 22:28:00 +0200

    Use fun-mode in exercise 02-05

--- src/nature_of_code/02_forces/exercise_02_05.clj
+++ src/nature_of_code/02_forces/exercise_02_05.clj
@@ -1,17 +1,24 @@
 (ns nature-of-code.02-forces.exercise-02-05
   (:require [quil.core :as q]
+            [quil.middleware :as md]
             [nature-of-code.mover :as m]
             [nature-of-code.vector :as v]))
 
-(def movers (atom ()))
+(defn setup []
+  (map
+   (fn [i] {:mass (+ 1 (rand-int 50))
+            :location [(* i 50) (* i 50)]
+            :velocity [0 0]
+            :acceleration [0 0]})
+   (range 10)))
 
-(loop [times 10]
-  (when (> times 0)
-    (swap! movers #(conj % {:mass (+ 1 (rand-int 50))
-                            :location [(* times 50) (* times 50)]
-                            :velocity [0 0]
-                            :acceleration [0 0]}))
-    (recur (dec times))))
+(defn compute-position [{:keys [acceleration velocity location] :as mover}]
+  (let [velocity (v/add acceleration velocity)
+        location (v/add velocity location)]
+    (-> mover
+        (assoc :acceleration [0 0])
+        (assoc :velocity velocity)
+        (assoc :location location))))
 
 (defn liquid [x y w h c]
   {:location [x y]
@@ -30,27 +37,34 @@
               (* c speed speed))
       [0 0])))
 
-(defn update-mover [mover width height]
-  (let [wind [0.1 0]]
-    (-> mover
-        (m/apply-force (gravity mover))
-        (m/apply-force (drag mover (liquid 0 (/ (q/height) 2) (q/width) (/ (q/height) 2) 1)))
-        (m/compute-position)
-        (m/keep-inside width height))))
+(defn update-mover [mover]
+  (-> mover
+      (m/apply-force (gravity mover))
+      (m/apply-force (drag mover (liquid 0 (/ (q/height) 2) (q/width) (/ (q/height) 2) 1)))
+      compute-position
+      m/keep-inside))
 
-(defn draw []
+(defn update-state [movers]
+  (map update-mover movers))
+
+(defn draw [movers]
   (q/clear)
   (q/background 255)
   (q/fill 50 100 255)
   (q/rect 0 (/ (q/height) 2) (q/width) (/ (q/height) 2))
   (q/fill 255 0 255)
-  (doseq [{:keys [mass velocity] [x y] :location}
-          (swap! movers #(map (fn [mover] (update-mover mover (q/height) (q/width))) %))]
+  (doseq [{:keys [mass velocity] [x y] :location} movers]
     (q/ellipse x y mass mass)))
 
 (defn run []
   (q/defsketch fluid-resistance
     :title "fluid-resistance"
     :settings #(q/smooth 2)
+    :setup setup
+    :update update-state
     :draw draw
-    :size [500 500]))
+    :size [500 500]
+    :middleware [md/fun-mode]))
+
+
+
