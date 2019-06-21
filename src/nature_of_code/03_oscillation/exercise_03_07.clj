13675cbc7c070a92a1b31b996ed89c28e06dc6fd
Thomas Sojka <thomas.sojka@comsysto.com>
2019-06-21 10:11:30 +0200

    use fun-mode for insect sketch

--- src/nature_of_code/03_oscillation/exercise_03_07.clj
+++ src/nature_of_code/03_oscillation/exercise_03_07.clj
@@ -3,64 +3,61 @@
             [quil.core :as q]
             [quil.middleware :as md]))
 
-(def oscilators (atom '({:angle [0 0]
-                         :velocity [0 0.03]
-                         :amplitude [-30 -30]
-                         :location [-50 -50]}
-                        {:angle [0 0]
-                         :velocity [0 -0.03]
-                         :amplitude [30 30]
-                         :location [50 -50]}
-
-                        {:angle [0 0]
-                         :velocity [0 0.03]
-                         :amplitude [0 -30]
-                         :location [-100 0]}
-                        {:angle [0 0]
-                         :velocity [0 0.03]
-                         :amplitude [0 30]
-                         :location [100 0]}
-
-                        {:angle [0 0]
-                         :velocity [0 0.03]
-                         :amplitude [-30 -30]
-                         :location [-50 50]}
-                        {:angle [0 0]
-                         :velocity [0 0.03]
-                         :amplitude [30 30]
-                         :location [50 50]})))
-
 (defn setup []
-  )
+  '({:angle [0 0]
+     :velocity [0 0.03]
+     :amplitude [-30 -30]
+     :location [-50 -50]}
+    {:angle [0 0]
+     :velocity [0 -0.03]
+     :amplitude [30 30]
+     :location [50 -50]}
+    {:angle [0 0]
+     :velocity [0 0.03]
+     :amplitude [0 -30]
+     :location [-100 0]}
+    {:angle [0 0]
+     :velocity [0 0.03]
+     :amplitude [0 30]
+     :location [100 0]}
+    {:angle [0 0]
+     :velocity [0 0.03]
+     :amplitude [-30 -30]
+     :location [-50 50]}
+    {:angle [0 0]
+     :velocity [0 0.03]
+     :amplitude [30 30]
+     :location [50 50]}))
 
-(defn draw []
+(defn draw-foot [{[a1 a2] :angle [am1 am2] :amplitude [l1 l2] :location}]
+  (let [x (+ l1 (* am1 (q/cos a1)))
+        y (+ l2 (* am2 (q/cos a2)))]
+    (q/push-matrix)
+    (q/stroke 0)
+    (q/fill 175)
+    (q/translate (/ (q/width) 2) (/ (q/height) 2))
+    (q/line 0 0 x y)
+    (q/ellipse x y 20 20)
+    (q/pop-matrix)))
+
+(defn draw [state]
   (q/background 255)
-  (doseq [{:keys [angle velocity amplitude location]}
-          (swap! oscilators
-                 #(map (fn [o]
-                         (update o :angle
-                                 (fn [a]
-                                   (v/add a (:velocity o)))))
-                       %))]
-    (let [[a1 a2] angle
-          [am1 am2] amplitude
-          [l1 l2] location
-          x (+ l1 (* am1 (q/cos a1)))
-          y (+ l2 (* am2 (q/cos a2)))]
-      (q/push-matrix)
-      (q/stroke 0)
-      (q/fill 175)
-      (q/translate (/ (q/width) 2) (/ (q/height) 2))
-      (q/line 0 0 x y)
-      (q/ellipse x y 20 20)
-      (q/pop-matrix))))
+  (doseq [foot state]
+    (draw-foot foot)))
+
+(defn update-foot [{:keys [velocity] :as foot}]
+  (update foot :angle v/add velocity))
+
+(defn update-state [state]
+  (map update-foot state))
 
 (defn run []
   (q/defsketch insect
     :title "insect"
     :settings #(q/smooth 2)
-    :middleware [md/pause-on-error]
+    :middleware [md/pause-on-error md/fun-mode]
     :setup setup
+    :update update-state
     :draw draw
     :features [:no-bind-output]
     :size [500 500]))
