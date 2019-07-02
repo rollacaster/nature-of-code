fcc60f69c74d8cc1499a5581e6d6b640d64703b6
Thomas Sojka <thomas.sojka@comsysto.com>
2019-07-02 06:47:31 +0200

    Use fun-mode for custom wave

--- src/nature_of_code/03_oscillation/exercise_03_10.clj
+++ src/nature_of_code/03_oscillation/exercise_03_10.clj
@@ -2,42 +2,50 @@
   (:require [quil.core :as q]
             [quil.middleware :as md]))
 
-(def waves (atom '({:amplitude [300 200]
-                    :angleVel 0.1
-                    :startAngle 0
-                    :location [50 50]}
-                   {:amplitude [500 50]
-                    :angleVel 0.3
-                    :startAngle 0
-                    :location [50 300]})))
-
-(defn draw-wave [wave]
-  (let [{:keys [angleVel startAngle]
-         [l1 l2] :location
-         [a1 a2] :amplitude} wave]
-    (let [angle (atom startAngle)]
-      (doseq [i (range (/ a1 24))
-              :let [x (* i 24)]]
-        (let [y (q/map-range (q/sin @angle) -1 1 0 a2)]
-          (q/stroke 0)
-          (q/fill 0 50)
-          (q/ellipse (+ l1 x) (+ l2 y) 48 48)
-          (swap! angle #(+ % angleVel)))))
-    (update wave :startAngle #(+ % 0.02))))
-
 (defn setup []
-  )
+  (list
+   (let [element-counts (/ (q/width) 24)
+         angle-vel 0.02]
+     {:amplitude 200
+      :angle-vel angle-vel
+      :elements (map #(vector (* % 24) (* % angle-vel)) (range element-counts))
+      :location [0 (* 0.1 (q/height))]})
+   (let [element-counts (/ (q/width) 48)
+         angle-vel 0.3]
+     {:amplitude 50
+      :angle-vel angle-vel
+      :elements (map #(vector (* % 24) (* % angle-vel)) (range element-counts))
+      :location [0 (* 0.8 (q/height))]})))
 
-(defn draw []
+(defn update-element [angle-vel [x y]]
+  [(mod (+ 1 x) (q/width)) (+ angle-vel y)])
+
+(defn update-wave [{:keys [angle-vel] :as wave}]
+  (update wave :elements (partial map (partial update-element angle-vel))))
+
+(defn update-state [waves]
+  (map update-wave waves))
+
+(defn draw-wave [{:keys [angleVel elements amplitude]
+                  [l1 l2] :location}]
+  (doseq [[x angle] elements]
+    (let [y (q/map-range (q/sin angle) -1 1 0 amplitude)]
+      (q/stroke 0)
+      (q/fill 0 50)
+      (q/ellipse (+ l1 x) (+ l2 y) 48 48))))
+
+(defn draw [waves]
   (q/background 255)
-  (doseq [wave (swap! waves (fn [waves] (map draw-wave waves)))]))
+  (doseq [wave waves]
+    (draw-wave wave)))
 
 (defn run []
   (q/defsketch wave
     :title "wave"
     :settings #(q/smooth 2)
-    :middleware [md/pause-on-error]
+    :middleware [md/pause-on-error md/fun-mode]
     :setup setup
     :draw draw
+    :update update-state
     :features [:no-bind-output]
     :size [700 500]))
