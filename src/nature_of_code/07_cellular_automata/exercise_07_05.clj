b67d6349891d023f9e689d596902e3546ded7399
Thomas Sojka <thomas.sojka@comsysto.com>
2019-07-31 19:14:30 +0200

    Generate all 1-dimensional ca's

---
+++ src/nature_of_code/07_cellular_automata/exercise_07_05.clj
@@ -0,0 +1,71 @@
+(ns nature-of-code.07-cellular-automata.exercise-07-05
+  (:require [quil.core :as q]
+            [quil.middleware :as md]
+            [clojure.string :as s]))
+
+(defn setup-cells []
+  (let [w 5
+        cell-count (/ 700 w)]
+    (map-indexed (fn [idx x] (if (= idx (q/ceil (/ cell-count 2))) 1 0))
+                 (range cell-count))))
+
+(defn setup []
+  {:cells (setup-cells)
+   :ruleset [0 0 0 0 0 0 0 0]
+   :generation 0})
+
+(defn rules [ruleset a b c]
+  (get ruleset (q/unbinary (str a b c))))
+
+(defn increase-ruleset [ruleset]
+  (into []
+      (map
+       #(Character/digit % 10)
+       (take-last 8 (q/binary (inc (q/unbinary (s/join ruleset))))))))
+
+(defn reset-state [state]
+  (-> state
+      (assoc :generation 0)
+      (assoc :cells (setup-cells))
+      (assoc :ruleset (increase-ruleset (:ruleset state)))))
+
+(defn update-automata [{:keys [cells ruleset] :as state}]
+  (-> state
+      (assoc :cells (map-indexed (fn [idx x]
+                                   (cond (= idx 0) 0
+                                         (= idx (- (count cells) 1)) 0
+                                         :else (rules ruleset
+                                                      (nth cells (dec idx))
+                                                      (nth cells idx)
+                                                      (nth cells (inc idx)))))
+                                 cells))
+      (update :generation inc)))
+
+(defn update-state [{:keys [generation ruleset] :as state}]
+  (if (> generation (/ (q/height) 5))
+    (if (= ruleset [1 1 1 1 1 1 1 1])
+      (q/exit)
+      (do
+        (q/save (str "images/" (s/join ruleset)))
+        (reset-state state)))
+    (update-automata state)))
+
+(defn draw [{:keys [cells generation]}]
+  (doall
+   (map-indexed
+    (fn [idx cell]
+      (if (= cell 1) (q/fill 0) (q/fill 255))
+      (q/rect (* idx 5) (* 5 generation) 5 5))
+    cells)))
+
+(defn run []
+  (q/defsketch ca-images
+    :title "ca-images"
+    :display 1
+    :settings #(q/smooth 2)
+    :middleware [md/pause-on-error md/fun-mode]
+    :setup setup
+    :draw draw
+    :update update-state
+    :features [:no-bind-output]
+    :size [700 500]))
