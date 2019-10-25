4a16d0eda75025f1370c5e1ed21bc747be7f85a8
Thomas Sojka <thomas.sojka@comsysto.com>
2019-10-25 18:51:08 +0200

    Fix multipendulum sketch

--- src/nature_of_code/03_oscillation/exercise_03_12.clj
+++ src/nature_of_code/03_oscillation/exercise_03_12.clj
@@ -4,58 +4,54 @@
             [nature-of-code.vector :as v]))
 
 (defn create-pendulum [origin r]
-  {:location [] :origin origin :r r :angle (/ (Math/PI) 4.0) :damping 0.995
-   :aVelocity 0.0 :aAcceleration 0.0})
+  {:location [0 0] :origin origin :r r :angle (/ q/PI 4.0) :damping 0.995
+   :a-velocity 0.0 :a-acceleration 0.0})
 
-(defn update-pendulum [{:keys [angle aVelocity r damping origin] :as pendulum}]
+(defn update-pendulum [{:keys [angle a-velocity r damping origin] :as pendulum}]
   (let [gravity 0.4
-        aAcceleration (* (/ (- gravity) r) (q/sin angle))
-        aVelocity (+ aVelocity aAcceleration)
-        angle (+ angle aVelocity)
+        a-acceleration (* (/ (- gravity) r) (q/sin angle))
+        a-velocity (+ a-velocity a-acceleration)
+        angle (+ angle a-velocity)
         location (v/add (vector (* r (q/sin angle)) (* r (q/cos angle))) origin)]
     (-> pendulum
-        (assoc :aAcceleration aAcceleration)
+        (assoc :a-acceleration a-acceleration)
         (assoc :angle angle)
         (assoc :origin origin)
         (assoc :location location)
-        (update :aVelocity (fn [_] (* aVelocity damping))))))
+        (assoc :a-velocity (* a-velocity damping)))))
 
-(defn draw-pendulum [{:keys [r angle origin location]}]
+(defn draw-pendulum [{:keys [r angle] [ox oy] :origin [x y] :location}]
   (q/stroke 0)
-  (q/line (first origin) (second origin) (first location) (second location))
-  (q/ellipse (first location) (second location) 16 16))
-
-(def pendulums (atom (list
-                      (create-pendulum [350 0] 100.0)
-                      (create-pendulum [350 100] 100.0))))
-
-
-(def pendulum (atom (create-pendulum [350 0] 100.0)))
-
+  (q/line ox oy x y)
+  (q/ellipse x y 16 16))
 
 (defn setup []
-  )
+  (list
+   (create-pendulum [(/ (q/width) 2) 0] 100.0)
+   (create-pendulum [(/ (q/width) 2) 100] 100.0)
+   (create-pendulum [(/ (q/width) 2) 200] 100.0)))
 
-(defn update-pendulums [pendulums]
-  (reduce (fn [pendulums pendulum]
-            (conj pendulums
-                  (update-pendulum (assoc pendulum :origin (if (= (:origin pendulum) [350 0])
-                                                             (:origin pendulum)
-                                                             (:location (last pendulums)))))))
-          ()
-          pendulums))
+(defn update-state [pendulums]
+  (map-indexed
+   (fn [idx pendulum]
+     (update-pendulum (if (= idx 0)
+                        pendulum
+                        (assoc pendulum :origin (:location (nth pendulums (dec idx)))))))
+   pendulums))
 
-(defn draw []
+(defn draw [pendulums]
   (q/background 255)
-  (doseq [pendulum (swap! pendulums update-pendulums)]
+  (doseq [pendulum pendulums]
     (draw-pendulum pendulum)))
 
 (defn run []
   (q/defsketch multi-pendulum
     :title "multi-pendulum"
     :settings #(q/smooth 2)
-    :middleware [md/pause-on-error]
+    :middleware [md/pause-on-error md/fun-mode]
+    :update update-state
     :setup setup
     :draw draw
     :features [:no-bind-output]
+    :display 1
     :size [700 500]))
