85049b5dc60c75a23b69c823315346c3e92b314f
Thomas Sojka <thomas.sojka@comsysto.com>
2019-10-26 18:28:18 +0200

    Use fun mode for 3.16

--- src/nature_of_code/03_oscillation/exercise_03_16.clj
+++ src/nature_of_code/03_oscillation/exercise_03_16.clj
@@ -14,46 +14,46 @@
         stretch (- d (:len spring))]
     (m/apply-force bob (v/mult (v/normalize force) (* -1 k stretch)))))
 
-(def springs (atom [{:spring (create-spring 350 0 100)
-                     :bob (m/create-mover 50 [300 50])}
-                    {:spring (create-spring 250 0 100)
-                     :bob (m/create-mover 50 [200 50])}]))
 (defn setup []
-  )
+  [{:spring (create-spring 350 0 100)
+    :bob (m/create-mover 50 [300 50])}
+   {:spring (create-spring 250 0 100)
+    :bob (m/create-mover 50 [200 50])}])
 
-(defn draw []
+(defn update-state [state]
+  (reduce (fn [springs {:keys [spring bob]}]
+            (conj springs
+                  {:bob
+                   (-> bob
+                       (m/apply-force [0 1])
+                       (connect spring)
+                       (m/compute-position))
+                   :spring (if-let [last-bob-location (-> springs
+                                                          last
+                                                          :bob
+                                                          :location)]
+                             (assoc spring :location last-bob-location)
+                             spring)}))
+          []
+          state))
+
+(defn draw [state]
   (q/background 255)
   (q/rect-mode :center)
-  (doseq [{:keys [spring bob]}
-          (swap! springs
-                 (fn [springs]
-                   (reduce (fn [springs {:keys [spring bob]}]
-                             (conj springs
-                                   {:bob
-                                    (-> bob
-                                        (m/apply-force [0 1])
-                                        (connect spring)
-                                        (m/compute-position))
-                                    :spring (if-let [last-bob-location (-> springs
-                                                                  last
-                                                                  :bob
-                                                                  :location)]
-                                              (assoc spring :location last-bob-location)
-                                             spring)}))
-                           []
-                           springs)))]
+  (doseq [{:keys [spring bob]} state]
     (let [{:keys [len] [a1 a2] :location} spring
           {:keys [mass] [x y] :location} bob]
       (q/line x y a1 a2)
       (q/rect a1 a2 10 10)
       (q/ellipse x y mass mass))))
 
-(defn run []
-  (q/defsketch multiple-springs
-    :title "multiple-springs"
-    :settings #(q/smooth 2)
-    :middleware [md/pause-on-error]
-    :setup setup
-    :draw draw
-    :features [:no-bind-output]
-    :size [700 500]))
+(q/defsketch multiple-springs
+  :title "multiple-springs"
+  :settings #(q/smooth 2)
+  :middleware [md/pause-on-error md/fun-mode]
+  :setup setup
+  :draw draw
+  :display 1
+  :update update-state
+  :features [:no-bind-output]
+  :size [700 500])
