390167e92fbe0d3722ed332366c287c01093a3f8
Thomas Sojka <thomas.sojka@comsysto.com>
2019-10-26 18:38:22 +0200

    Use  fun mode for particle force

--- src/nature_of_code/04_particle-systems/exercise_04_01.clj
+++ src/nature_of_code/04_particle-systems/exercise_04_01.clj
@@ -21,37 +21,36 @@
 (defn apply-force [particle force]
   (update particle :acceleration #(v/add % force)))
 
-(defn display [{:keys [lifespan] [x y] :location :as particle}]
+(defn display [{:keys [lifespan] [x y] :location}]
   (q/stroke 0 lifespan)
   (q/fill 0 lifespan)
-  (q/ellipse x y 8 8)
-  particle)
+  (q/ellipse x y 8 8))
 
 (defn is-dead [{:keys [lifespan]}]
   (< lifespan 0.0))
 
-(defn run [particle]
-  (-> particle
-      (apply-force [-0.01 0.01])
-      update-particle
-      display))
-
 (defn setup []
-  )
+  (create-particle [250 250]))
 
-(def particle (atom (create-particle [250 250])))
+(defn update-state [particle]
+  (if (is-dead particle)
+    (create-particle [250 250])
+    (-> particle
+        (apply-force [-0.01 0.01])
+        update-particle)))
 
-(defn draw []
+(defn draw [particle]
   (q/background 255)
-  (when (is-dead (swap! particle run))
-    (def particle (atom (create-particle [250 250])))))
+  (display particle))
 
 (defn run []
   (q/defsketch particle-force
     :title "particle-force"
     :settings #(q/smooth 2)
-    :middleware [md/pause-on-error]
+    :middleware [md/pause-on-error md/fun-mode]
     :setup setup
     :draw draw
+    :update update-state
     :features [:no-bind-output]
+    :display 1
     :size [700 500]))
