1fecf87062427a83edcc73b9f6fb4671627451a8
Thomas Sojka <thomas@Thomass-MBP.fritz.box>
2020-03-11 06:40:41 +0100

    Fix shutter in pieces

--- src/nature_of_code/04_particle_systems/exercise_04_06.clj
+++ src/nature_of_code/04_particle_systems/exercise_04_06.clj
@@ -1,115 +1,89 @@
 (ns nature-of-code.04-particle-systems.exercise-04-06
-  (:require [nature-of-code.vector :as v]
-            [quil.core :as q]
-            [quil.middleware :as md]))
+  (:require [quil.core :as q]
+            [quil.middleware :as md]
+            [nature-of-code.mover :as m]
+            [nature-of-code.vector :as v]))
 
-(def particle-systems (atom ()))
+;; Create a simulation of an object shattering into many pieces. How can you
+;; turn one large shape into many small particles? What if there are several
+;; large shapes on the screen and they shatter when you click on them?
+(defn setup []
+  (q/no-stroke)
+  (map
+   (fn [[x0 y0]]
+     {:particles (for [x (range 0 100 10)
+                       y (range 0 100 10)]
+                   (-> (m/create-mover 10 [(+ x0 x) (+ y0 y)])
+                       (assoc :lifespan 255)))
+      :exploding false})
+   (map
+    (fn [x y] [x y])
+    (range 50 (- (q/width) 50) (/ (- (q/width) 50) 4))
+    (range 50 (- (q/height) 50) (/ (- (q/height) 50) 4)))))
 
-(defn create-particle [[x y]]
-  (let [x (+ (- (rand 100) 50) x)
-        y (+ (- (rand 100) 50) y)]
-    {:location [x y]
-     :velocity [(cond (< x 0) (- (rand 1) 1)
-                      (>= x 0) (rand 1))
-                (cond (< y 0) (- (rand 1) 1)
-                      (>= y 0) (rand 1))]
-     :acceleration [0 0]
-     :lifespan 255.0
-     :aAcceleration 0.0
-     :aVelocity 0.1
-     :angle 0.0}))
+(defn is-dead [{:keys [lifespan]}] (< lifespan 0.0))
 
-(defn update-particle [{:keys [acceleration velocity location lifespan
-                               aVelocity aAcceleration angle]
-                        :as particle}]
-  (let [velocity (v/add velocity acceleration)
-        location (v/add velocity location)
-        lifespan (- lifespan 2.0)
-        aVelocity (+ aVelocity aAcceleration)
-        angle (+ aVelocity angle)]
-    (-> particle
-        (assoc :velocity velocity)
-        (assoc :location location)
-        (assoc :acceleration [0 0])
-        (assoc :lifespan lifespan)
-        (assoc :aVelocity aVelocity)
-        (assoc :angle (+ aVelocity angle))
-        (assoc :aAcceleration 0))))
+(defn compute-explosion-force [particle particles]
+  (let [[x1 y1] (:location (first particles))
+        [x2 y2] (:location (last particles))
+        mid [(+ x1 (/ (- x2 x1) 2)) (+ y1 (/ (- y2 y1) 2))]
+        [d1 d2] (v/mult (v/normalize (v/sub (:location particle) mid)) 50)]
+    [d1 d2]))
 
-(defn apply-force [force particle]
-  (update particle :acceleration #(v/add % force)))
+(defn update-particle-system [{:keys [particles exploding]
+                               :as particle-system}]
+  (if exploding
+    (assoc particle-system :particles (->> particles
+                                           (map (fn [particle]
+                                                  (-> particle
+                                                      (update :lifespan dec)
+                                                      m/compute-position)))
+                                           (remove is-dead)))
+    particle-system))
 
-(defn display [{:keys [lifespan angle] [x y] :location :as particle}]
+(defn update-state [particle-systems]
+  (map update-particle-system particle-systems))
+
+(defn draw-particle [{:keys [lifespan angle mass]
+                      [x y] :location
+                      :as particle}]
   (q/push-matrix)
   (q/rect-mode :center)
   (q/translate x y)
   (q/rotate angle)
-  (q/stroke 0 lifespan)
   (q/fill 127 0 0 lifespan)
-  (q/rect 0 0 8 8)
+  (q/rect 0 0 mass mass)
   (q/pop-matrix)
   particle)
 
-(defn is-dead [{:keys [lifespan]}]
-  (< lifespan 0.0))
+(defn draw [particle-systems]
+  (q/background 255)
+  (doseq [particle-system particle-systems]
+    (doseq [particle (:particles particle-system)]
+      (draw-particle particle))))
 
-(defn run [particle]
-  (-> particle
-      update-particle
-      display))
+(defn is-particle-system-clicked [{:keys [particles]} x y]
+  (let [[x1 y1] (:location (first particles))
+        [x2 y2] (:location (last particles))]
+    (and (> x x1) (> y y1) (< x x2) (< y y2))))
 
-(defn setup []
-  )
-
-(defn add-origin [origin {:keys [location] :as particle}]
-  (assoc particle :location (v/add location origin)))
-
-(defn run-particle-system [particles x y particle-count]
-  (doall
-   (->> (if (> particle-count 1)
-          (conj particles (add-origin [x y] (create-particle [0 0])))
-          particles)
-        (map run)
-        (remove is-dead))))
-
-(defn create-particle-system [x y particle-count]
-  {:location [x y]
-   :particle-count particle-count
-   :exploded 0.0
-   :particles []})
-
-(defn run-particle-systems [{:keys [particles particle-count exploded]
-                             [x y] :location :as particle-system}]
-  (if (>= exploded 100)
-    (do
-      (q/clear)
-      (-> particle-system
-          (update :particle-count dec)
-          (assoc :particles (run-particle-system particles x y (dec particle-count)))))
-    (do
-      (q/fill 127 0 0)
-      (q/rect x y 100 100)
-      (update particle-system :exploded inc))))
-
-(defn draw []
-  (q/fill 127)
-  (doall (swap! particle-systems #(->> %
-                                       (map run-particle-systems)
-                                       (remove (fn [{:keys [particle-count]}]
-                                                 (<= particle-count -127)))))))
-
-(defn on-mouse-pressed []
-  (swap! particle-systems #(conj % (create-particle-system (q/mouse-x)
-                                                           (q/mouse-y)
-                                                           127))))
+(defn mouse-pressed [particle-systems {:keys [x y]}]
+  (map (fn [particle-system]
+         (-> particle-system
+             (update :particles #(map (fn [particle] (m/apply-force
+                                                     particle
+                                                     (compute-explosion-force particle %))) %))
+             (assoc :exploding (or (:exploding particle-system) (is-particle-system-clicked particle-system x y)))))
+       particle-systems))
 
 (defn run []
-  (q/defsketch particle-system-mouse
-    :title "particle-system-mouse"
+  (q/defsketch shatter-in-pieces
+    :title "shatter-in-pieces"
     :settings #(q/smooth 2)
-    :middleware [md/pause-on-error]
+    :middleware [md/pause-on-error md/fun-mode]
     :setup setup
     :draw draw
+    :update update-state
     :features [:no-bind-output]
-    :mouse-pressed on-mouse-pressed
+    :mouse-pressed mouse-pressed
     :size [700 500]))
