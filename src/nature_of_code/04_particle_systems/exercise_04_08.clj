4a48a6a0b33e9b5b09ae1bd3a6bb375fc459aabc
Thomas Sojka <thomas@Thomass-MBP.fritz.box>
2020-03-13 08:03:11 +0100

    Use fun-mode for 04-08

--- src/nature_of_code/04_particle_systems/exercise_04_08.clj
+++ src/nature_of_code/04_particle_systems/exercise_04_08.clj
@@ -1,58 +1,26 @@
 (ns nature-of-code.04-particle-systems.exercise-04-07
-  (:require [nature-of-code.vector :as v]
+  (:require [nature-of-code.mover :as m]
             [quil.core :as q]
             [quil.middleware :as md]))
 
-(def particle-system (atom {:particles ()
-                            :confetti ()
-                            :origin [200 200]}))
-(defn add-particle [ps]
-  (update ps :particles #(conj % (apply (if (> (q/random 1) 0.5)
-                                          create-particle
-                                          create-confetti)
-                                        (list (:origin ps))))))
+(defn setup []
+  {:particles ()
+   :origin [(/ (q/width) 2) (/ (q/height) 2)]})
 
-
-(defn run-particle-system [{:keys [particles confetti] :as ps}]
-  (doseq [particle particles]
-    (-> particle
-        display-particle))
-  (-> ps
-      (update :particles #(map update-particle %))
-      (update :particles #(remove is-dead %))
-      (update :confetti #(map update-particle %))
-      (update :confetti #(remove is-dead %))))
+(defn create-particle [location]
+  (-> (m/create-mover 100 location)
+      (assoc :velocity [(- (rand 2) 1) (- (rand 2) 2)])
+      (assoc :acceleration [0 0.05])
+      (assoc :a-acceleration 0.1)
+      (assoc :lifespan 255)))
 
 (defn create-confetti [location]
   (-> (create-particle location)
       (assoc :type :confetti)))
 
-(defn create-particle [location]
-  {:location location
-   :velocity [(- (rand 2) 1) (- (rand 2) 2)]
-   :acceleration [0 0.05]
-   :lifespan 255.0
-   :aAcceleration 0.1
-   :aVelocity 0.0
-   :angle 0.0})
+(defmulti draw-particle :type)
 
-(defn update-particle [{:keys [acceleration velocity location lifespan
-                               aVelocity aAcceleration angle] :as particle}]
-  (let [velocity (v/add velocity acceleration)
-        location (v/add velocity location)
-        lifespan (- lifespan 2.0)
-        aVelocity (+ aVelocity aAcceleration)
-        angle (+ aVelocity angle)]
-    (-> particle
-        (assoc :velocity velocity)
-        (assoc :location location)
-        (assoc :lifespan lifespan)
-        (assoc :aVelocity aVelocity)
-        (assoc :angle angle)
-        (assoc :aAcceleration 0.0))))
-
-(defmulti display-particle :type)
-(defmethod display-particle :confetti [{:keys [lifespan angle] [x y] :location :as particle}]
+(defmethod draw-particle :confetti [{:keys [lifespan angle] [x y] :location :as particle}]
   (q/rect-mode :center)
   (q/fill 175 lifespan)
   (q/stroke 0 lifespan)
@@ -62,29 +30,36 @@
   (q/rect 0 0 8 8)
   (q/pop-matrix)
   particle)
-(defmethod display-particle :default [{:keys [lifespan] [x y] :location :as particle}]
+
+
+(defmethod draw-particle :default [{:keys [lifespan] [x y] :location :as particle}]
   (q/stroke 0 lifespan)
   (q/fill 0 lifespan)
   (q/ellipse x y 8 8)
   particle)
 
-(defn is-dead [{:keys [lifespan]}]
-  (< lifespan 0.0))
+(defn is-dead [{:keys [lifespan]}] (< lifespan 0.0))
 
-(defn setup []
-  )
+(defn decrease-lifespan [particle]
+  (update particle :lifespan (comp dec dec)))
 
-(defn draw []
+(defn update-state [{:keys [origin] :as ps}]
+  (-> ps
+      (update :particles #(conj % (if (> (q/random 1) 0.5) (create-particle origin) (create-confetti origin))))
+      (update :particles #(map (comp m/compute-position decrease-lifespan) %))
+      (update :particles #(remove is-dead %))))
+
+(defn draw [{:keys [particles]}]
   (q/background 255)
-  (swap! particle-system (comp
-                          add-particle
-                          run-particle-system)))
+  (doseq [particle particles]
+    (draw-particle particle)))
 
 (defn run []
   (q/defsketch particle
     :title "particle"
     :settings #(q/smooth 2)
-    :middleware [md/pause-on-error]
+    :middleware [md/pause-on-error md/fun-mode]
+    :update update-state
     :setup setup
     :draw draw
     :features [:no-bind-output]
