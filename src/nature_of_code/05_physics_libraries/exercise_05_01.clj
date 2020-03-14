2242dc70ceee04376a1c63f9603112e0ec1258bd
Thomas Sojka <thomas@Thomass-MBP.fritz.box>
2020-03-14 21:54:43 +0100

    Use fun-mode for 05-01

--- src/nature_of_code/05_physics_libraries/exercise_05_01.clj
+++ src/nature_of_code/05_physics_libraries/exercise_05_01.clj
@@ -4,13 +4,20 @@
             [quil.middleware :as md]))
 
 (def world (b/new-world [0 100]))
-(def boxes (atom ()))
+
+(defn setup []
+  ())
 
 (defn create-box [world x y]
   (let [body (b/body! world {:position [x y]} {:shape (b/box 8 8) :restitution 0.5})]
     {:body body :w 16 :h 16}))
 
-(defn display-box [{:keys [body w h]}]
+(defn update-state [boxes]
+  (if (q/mouse-pressed?)
+    (conj boxes (create-box world (q/mouse-x) (q/mouse-y)))
+    boxes))
+
+(defn draw-box [{:keys [body w h]}]
   (let [[[x y]] (b/world-coords (b/fixture-of body))
         angle (b/angle body)]
     (q/push-matrix)
@@ -22,22 +29,20 @@
     (q/rect 0 0 w h)
     (q/pop-matrix)))
 
-(defn setup [])
-
-(defn draw []
+(defn draw [boxes]
   (q/clear)
   (q/background 127)
   (b/step! world (/ 1.0 60.0))
-  (when (q/mouse-pressed?)
-    (swap! boxes conj (create-box world (q/mouse-x) (q/mouse-y))))
-  (doall (map display-box @boxes)))
+  (doseq [box boxes]
+    (draw-box box)))
 
 (defn run []
   (q/defsketch physics
     :title "physics"
     :settings #(q/smooth 2)
-    :middleware [md/pause-on-error]
+    :middleware [md/pause-on-error md/fun-mode]
     :setup setup
     :draw draw
+    :update update-state
     :features [:no-bind-output]
     :size [700 500]))
